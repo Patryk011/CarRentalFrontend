5b7b4bebcc758ba9fd171e5cd45f28a357660362
Patryk <patryknartowski@wp.pl>
2024-10-23 23:17:52 +0200

    config

--- src/App.vue
+++ src/App.vue
@@ -1,5 +1,56 @@
 <script setup></script>
 
-<template>Welcome</template>
+<template>
+  <div class="app-container">
+    <header class="main-header">
+      <div class="header-wrapper">
+        <div class="header-logo">
+          <a href="/">
+            <img src="./assets/logo.png" alt="" />
+          </a>
+        </div>
+      </div>
+    </header>
+    <footer>
+      &copy; 2024 Wypożyczalnia samochodów. Wszelkie prawa zastrzeżone.
+    </footer>
+  </div>
+</template>
 
-<style scoped></style>
+<style lang="scss" scoped>
+* {
+  margin: 0;
+  padding: 0;
+  box-sizing: border-box;
+}
+
+.app-container {
+  position: relative;
+  width: 100%;
+  height: 100%;
+
+  .main-header {
+    background-color: #3b6398;
+    width: 100%;
+    position: fixed;
+    top: 0;
+    left: 0;
+
+    .header-wrapper {
+      display: flex;
+    }
+  }
+
+  footer {
+    width: 100%;
+    display: flex;
+    background-color: #333;
+    position: fixed;
+    color: white;
+    text-align: center;
+    padding: 40px;
+    bottom: 0;
+    left: 0;
+  }
+}
+</style>
