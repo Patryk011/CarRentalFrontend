6465f5de7791481884b3ea3f0e74ab5cc0be81eb
Patryk <patryknartowski@wp.pl>
2024-11-09 18:33:26 +0100

    handle error that doesn't exist

--- src/main.ts
+++ src/main.ts
@@ -4,11 +4,21 @@ import { createPinia } from "pinia";
 import router from "../src/router/router";
 import { initKeycloak } from "./services/keycloak.service";
 
-const pinia = createPinia();
+// Function that cleans the 'error' from a URL that doesn't exist.
+const cleanUrl = () => {
+  const urlHash = window.location.hash;
 
+  if (urlHash.includes("error=login_required")) {
+    const cleanedUrl = window.location.href.split("#")[0];
+    window.history.replaceState(null, "", cleanedUrl);
+  }
+};
+
+const pinia = createPinia();
 initKeycloak().then(() => {
   const app = createApp(App);
   app.use(pinia);
   app.use(router);
   app.mount("#app");
+  setTimeout(cleanUrl, 0);
 });
