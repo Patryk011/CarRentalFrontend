8db5d351adbe0695ca89e7050319aa6b9a2ea229
Patryk <patryknartowski@wp.pl>
2025-01-07 23:19:33 +0100

    client rent view

--- src/components/Client/views/Account.vue
+++ src/components/Client/views/Account.vue
@@ -1,3 +1,87 @@
 <template>
-  <div>ACCOUNT</div>
+  <div class="account-container"></div>
 </template>
+
+<script setup lang="ts">
+import { getToken } from "@/services/keycloak.service";
+import axios from "axios";
+import { ref } from "vue";
+
+const customer = ref({});
+const rentals = ref([]);
+
+const getCustomerInfo = async () => {
+  try {
+    const token = getToken();
+    if (!token) {
+      console.error("No Keycloak token");
+      return;
+    }
+
+    const response = await fetch("http://localhost:8081/api/users/me", {
+      method: "GET",
+      headers: {
+        Authorization: `Bearer ${token}`,
+        "Content-Type": "application/json",
+      },
+    });
+
+    if (!response.ok) {
+      throw new Error(`Error fetching user data: ${response.statusText}`);
+    }
+
+    customer.value = await response.json();
+    console.log("customer data:", customer.value);
+  } catch (err) {
+    console.error("Error fetching current customer:", err);
+  }
+};
+
+const fetchRentals = async () => {
+  try {
+    const token = getToken();
+
+    if (!token) {
+      console.error("No token");
+      return;
+    }
+
+    const response = await axios.get("http://localhost:8081/api/rentals/all", {
+      headers: {
+        Authorization: `Bearer ${token}`,
+        "Content-Type": "application/json",
+      },
+    });
+    rentals.value = response.data;
+  } catch (error) {
+    console.error("errror fetching rentals", error);
+  }
+};
+
+const cancelRental = async (rentalId: number) => {
+  try {
+    const token = getToken();
+
+    if (!token) {
+      console.error("no token");
+      return;
+    }
+    const response = await axios.patch(
+      `http://localhost:8081/api/rentals/${rentalId}/cancel`,
+      null,
+      {
+        headers: {
+          Authorization: `Bearer ${token}`,
+          "Content-Type": "application/json",
+        },
+      }
+    );
+
+    console.log("Rental canceled:", response.data);
+
+    await fetchRentals();
+  } catch (err) {
+    console.error("Error canceling rental:", err);
+  }
+};
+</script>
--- src/components/Client/views/RentalDetails.vue
+++ src/components/Client/views/RentalDetails.vue
@@ -80,7 +80,7 @@ async function fetchRental() {
 
 console.log(rental);
 
-async function getCurrentUser() {
+const getCurrentUser = async () => {
   try {
     const token = getToken();
     if (!token) {
@@ -90,24 +90,19 @@ async function getCurrentUser() {
 
     console.log(token);
 
-    const response = await fetch("http://localhost:8081/api/users/me", {
-      method: "GET",
+    const response = await axios.get("http://localhost:8081/api/users/me", {
       headers: {
         Authorization: `Bearer ${token}`,
         "Content-Type": "application/json",
       },
     });
 
-    if (!response.ok) {
-      throw new Error(`Error fetching user data: ${response.statusText}`);
-    }
-
-    user.value = await response.json();
-    console.log("User data:", user.value);
+    user.value = response.data;
+    console.log("user data:", user.value);
   } catch (err) {
     console.error("Error fetching current user:", err);
   }
-}
+};
 
 async function createPayment(paymentData) {
   try {
